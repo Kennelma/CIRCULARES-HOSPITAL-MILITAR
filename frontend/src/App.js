62cd627e269363aa1ba54bb795ca011b00b5ff08
Kennelma <kennelmahernandez@gmail.com>
2026-04-14 08:29:02 -0600

    PROYECTO DE CIRCULARES

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Proyecto en desarrollo 🚀</h1>
+      <p>Esta es la versión inicial de la aplicación. Próximamente se agregarán nuevas funcionalidades.</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Dashboard/dashboard.js
@@ -0,0 +1,11 @@
+
+export default function Dashboard() {
+
+  return (
+    <div className="flex min-h-screen bg-gray-950 text-white">
+
+
+    
+    </div>
+  );
+}
